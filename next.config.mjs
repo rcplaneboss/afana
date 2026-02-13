1b0fa5e5d111ae4e6abe061e44b1c44aff8880ec
rcplaneboss <qosimabdulrahman0@gmail.com>
2026-02-14 00:33:39 +0100

    add blog featies

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,15 @@
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
-  }
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**', // Allows any HTTPS domain
+      },
+    ],
+  },
 }
 
 export default nextConfig
